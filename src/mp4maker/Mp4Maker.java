0450639e5459318af27430188865817867c22688
Adam <Adam@AdamPC>
2016-07-11 15:58:10 -0400

    Add function to allow user input Directory name. Then new files will be added inside the new directory.

--- src/mp4maker/Mp4Maker.java
+++ src/mp4maker/Mp4Maker.java
@@ -1,36 +1,46 @@
 package mp4maker;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.io.*;
-
-/**
- * This app can create empty mp4 files from a list of file names provided by user.
- * @author Adam
- */
 
 public class Mp4Maker {
 
-    private ArrayList<String> names = new ArrayList<String>();
+    private ArrayList<String> fileNames = new ArrayList<String>();
     private Scanner sc = new Scanner(System.in);
     File f = null;
 
-    public void getNames() {
+    public void creator() {
+
+        System.out.println("Enter names of folders need to be created");
+        String folderNameInput = sc.nextLine();
 
         System.out.println("Enter names for mp4 files, type 0 when finished");
-        String userInput = sc.nextLine();
+        String fileNameInputer = sc.nextLine();
+
+        // Create new directory
+        try {
+            Path path = Paths.get("c:/temp/" + folderNameInput);
+            Files.createDirectories(path);
+            System.out.println("Folder created!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         // Add user's input to arraylist
         do {
-            names.add(userInput);
-            userInput = sc.nextLine();
-        } while (userInput.equalsIgnoreCase("0") == false);
+            fileNames.add(fileNameInputer);
+            fileNameInputer = sc.nextLine();
+        } while (fileNameInputer.equalsIgnoreCase("0") == false);
 
         // Create new empty files to specified path
-        for (int i = 0; i < names.size(); i++) {
+        for (int i = 0; i < fileNames.size(); i++) {
             try {
-                String path = "c:/temp/";
-                File f = new File(path + names.get(i) + ".mp4");
+                String pathOfDirectory = "c:/temp/" + folderNameInput + "/";
+                File f = new File(pathOfDirectory + fileNames.get(i) + ".mp4");
                 if (f.createNewFile()) {
                     System.out.println("File created");
                 } else {
@@ -40,11 +50,12 @@ public class Mp4Maker {
                 e.printStackTrace();
             }
         }
+
     }
 
     public static void main(String[] args) {
         Mp4Maker mp4Maker = new Mp4Maker();
-        mp4Maker.getNames();
+        mp4Maker.creator();
     }
 
 }
